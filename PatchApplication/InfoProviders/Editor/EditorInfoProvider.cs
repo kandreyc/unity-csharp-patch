b6baad4f1a9ccd8062d52834a7007b56d68948b9
kandreyc <andreychuk.karim@gmail.com>
2024-07-02 15:14:36 +0300

    UnityEditorPatch -> linux / windows / windows_arm support

--- PatchApplication/InfoProviders/Editor/EditorInfoProvider.cs
+++ PatchApplication/InfoProviders/Editor/EditorInfoProvider.cs
@@ -6,7 +6,8 @@ public static class EditorInfoProvider
 {
     public static bool TryGet(string lookupPath, out EditorInfo info)
     {
-        var contentPath = Path.Combine(lookupPath, "Unity.app", "Contents");
+        var contentPath = GetContentPath(lookupPath);
+        Console.WriteLine($"Content Path -> {contentPath}");
         var runtimePath = Path.Combine(contentPath, "NetCoreRuntime");
         var roslynPath = Path.Combine(contentPath, "DotNetSdkRoslyn");
         var sourceGeneratorLocations = new[]
@@ -34,6 +35,21 @@ public static class EditorInfoProvider
 
         return true;
     }
+
+#pragma warning disable CS0162 // Unreachable code detected
+    private static string GetContentPath(string lookupPath)
+    {
+#if WINDOWS
+        return Path.Combine(lookupPath, "Editor", "Data");
+#elif LINUX
+        return Path.Combine(lookupPath, "Editor", "Data");
+#elif MACOS
+        return Path.Combine(lookupPath, "Unity.app", "Contents");
+#endif
+
+        return lookupPath;
+    }
+#pragma warning restore CS0162
 
     private static bool IsDirectoriesExists(params string[] paths)
     {
